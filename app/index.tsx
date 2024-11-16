62c867bf286f43147bcd07446d12bb3e22dd8756
Neon <neonfamxxi@gmail.com>
2024-11-16 15:21:53 -0800

    IT FITS IN

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Modal } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
 import { SafeAreaView as SafeAreaViewREAL } from 'react-native-safe-area-context';
 
@@ -11,8 +11,9 @@ export default function Index() {
     enabled: false,
     latitude: 37.34924530703096,
     longitude: -121.93673748980143,
-    latitudeDelta: 0,
-    longitudeDelta: 0});
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  });
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -29,50 +30,44 @@ export default function Index() {
         latitude: coords.latitude,
         longitude: coords.longitude,
         latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        longitudeDelta: 0.0421
       });
     })();
   }, []);
 
-  if (!location) { return <Text>You're on your own man, I believe in you</Text>; }
+  if (!location.enabled) {
+    return <Text>You're on your own man, I believe in you</Text>;
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000000"
-      }}
-    >
-
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Text>fiewhuafwefiu</Text>
-          <MapView initialRegion={location} style={styles.map} provider="google" googleMapId="MAIN_MAP_UNIQUE_ID" showsTraffic >
-
-          </MapView>
-        </View>
+    <View style={styles.outerContainer}>
+      <SafeAreaView style={styles.container}>
+        <Text>fiewhuafwefiu</Text>
+        <MapView
+          initialRegion={location}
+          style={styles.map}
+          provider="google"
+          googleMapId="MAIN_MAP_UNIQUE_ID"
+          showsTraffic
+        />
       </SafeAreaView>
-      {/* <Modal>
-        <Text>hello this is in a modal</Text>
-      </Modal> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    backgroundColor: '#000000'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    // width: '100%',
-    // height: '100%',
+    alignItems: 'center'
   },
   map: {
-    // flex: 1,
-    // width: '100%',
-    // height: '100%',
-    ...StyleSheet.absoluteFillObject,
-  },
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  }
 });
