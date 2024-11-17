7fb8655f54a5ba30ac42333735a6a67cc89cce8e
theonelog <awni.alquraini@gmail.com>
2024-11-16 22:56:21 -0800

    made it convert to coords

--- algo.py
+++ algo.py
@@ -14,10 +14,22 @@ graph = ox.project_graph(graph, to_latlong=True)
 
 starting = ox.nearest_nodes(graph, -121.961640, 37.400800)
 ending = ox.nearest_nodes(graph, -121.937393, 37.352039)
-print(starting)
-print(ending)
+#print(starting)
+#print(ending)
 shortest_path_nodes = nx.shortest_path(graph, starting, ending, 'length')
+node_coordinates = []
+for node in shortest_path_nodes:
+    node_data = graph.nodes[node]
+    node_coordinates.append([node_data['x'], node_data['y']])
 
-print(shortest_path_nodes)
+print(node_coordinates)
 
 #fig, ax = ox.plot_graph(graph)
+#fig, ax = plt.subplots(figsize=(10, 10))
+
+#ox.plot_graph(graph, ax=ax, node_size=0, bgcolor='w')
+
+#path_edges = list(zip(shortest_path_nodes, shortest_path_nodes[1:]))
+#ox.plot_graph_route(graph, shortest_path_nodes, route_linewidth=4, route_color='r', ax=ax)
+
+#plt.show()
