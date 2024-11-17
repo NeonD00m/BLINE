3cd482ae897a9a91358a34f5c6e38c2d154084f2
theonelog <awni.alquraini@gmail.com>
2024-11-16 22:27:23 -0800

    updates

--- algo.py
+++ algo.py
@@ -1,11 +1,23 @@
 import osmnx as ox
+import networkx as nx
+import matplotlib.pyplot as plt
 
 place_name = "Santa Clara, Santa Clara County, California, United States"
 
-graph = ox.graph_from_place(place_name)
-graph = ox.project_graph(graph)
+graph = ox.graph_from_place(query=place_name, simplify=False)
+graph = ox.project_graph(graph, to_latlong=True)
 #graph = ox.convert_to_undirected(graph)
-graph = ox.consolidate_intersections(graph, 10, True, True, True)
+#graph = ox.consolidate_intersections(graph, 5.7, True, True, True)
 
-fig, ax = ox.plot_graph(graph)
+#graph.graph["simplified"] = False
+#s_graph = ox.simplify_graph(G=graph, edge_attrs_differ=None, remove_rings=False, track_merged=False)
 
+starting = ox.nearest_nodes(graph, -121.961640, 37.400800)
+ending = ox.nearest_nodes(graph, -121.937393, 37.352039)
+print(starting)
+print(ending)
+shortest_path_nodes = nx.shortest_path(graph, starting, ending, 'length')
+
+print(shortest_path_nodes)
+
+#fig, ax = ox.plot_graph(graph)
