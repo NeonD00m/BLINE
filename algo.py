99d192f07fab35c52bf01b80b887129a0a131236
theonelog <awni.alquraini@gmail.com>
2024-11-16 23:31:56 -0800

    added more modularity

--- algo.py
+++ algo.py
@@ -2,18 +2,19 @@ import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 
-place_name = "Santa Clara, Santa Clara County, California, United States"
+#coords = (-121.961640, 37.400800)
+coords = (37.400800, -121.961640)
+dest_coords = (37.352039, -121.937393)
 
-graph = ox.graph_from_place(query=place_name, simplify=False)
-graph = ox.project_graph(graph, to_latlong=True)
+graph = ox.graph_from_point(center_point=coords, dist=10000,simplify=False)
 #graph = ox.convert_to_undirected(graph)
 #graph = ox.consolidate_intersections(graph, 5.7, True, True, True)
 
 #graph.graph["simplified"] = False
 #s_graph = ox.simplify_graph(G=graph, edge_attrs_differ=None, remove_rings=False, track_merged=False)
-
-starting = ox.nearest_nodes(graph, -121.961640, 37.400800)
-ending = ox.nearest_nodes(graph, -121.937393, 37.352039)
+#fig, ax = ox.plot_graph(graph)
+starting = ox.nearest_nodes(graph, coords[1], coords[0])
+ending = ox.nearest_nodes(graph, dest_coords[1], dest_coords[0])
 #print(starting)
 #print(ending)
 shortest_path_nodes = nx.shortest_path(graph, starting, ending, 'length')
@@ -24,7 +25,6 @@ for node in shortest_path_nodes:
 
 print(node_coordinates)
 
-#fig, ax = ox.plot_graph(graph)
 #fig, ax = plt.subplots(figsize=(10, 10))
 
 #ox.plot_graph(graph, ax=ax, node_size=0, bgcolor='w')
